666ac3179cc8a06fc7d670b0b35683ef95da38fb
Krzysztof Pich <kontakt@pich.pl>
2019-06-24 23:14:33 +0200

    testing routing

--- html/index.php
+++ html/index.php
@@ -2,6 +2,24 @@
 
 require __DIR__.'/vendor/autoload.php';
 
-phpinfo();
+$mainRoute = new \Symfony\Component\Routing\Route(
+    '/{id}',
+    array('controller' => \Pich\Main\Controller\Index::class)
+);
 
-echo 'Hello world';
+$routes = new \Symfony\Component\Routing\RouteCollection();
+$routes->add('main', $mainRoute);
+
+// Init RequestContext object
+$context = new \Symfony\Component\Routing\RequestContext();
+$context->fromRequest(Symfony\Component\HttpFoundation\Request::createFromGlobals());
+
+// Init UrlMatcher object
+$matcher = new Symfony\Component\Routing\Matcher\UrlMatcher($routes, $context);
+
+// Find the current route
+$parameters = $matcher->match($context->getPathInfo());
+
+
+$request = \Symfony\Component\HttpFoundation\Request::createFromGlobals();
+var_dump($parameters);
