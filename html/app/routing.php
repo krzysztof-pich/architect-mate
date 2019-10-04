013f17238c363ff543494d21d948905b8c618906
Krzysztof Pich <kontakt@pich.pl>
2019-10-04 18:07:24 +0200

    routing fix

--- html/app/routing.php
+++ html/app/routing.php
@@ -3,7 +3,7 @@ return [
     'IndexRoute' => Di\create(\Pich\App\Router\Route::class)
         ->constructor('GET', '/', Di\get(\Pich\Main\Action\Index::class)),
     'Dispatcher' => Di\create(\Pich\App\Router\Dispatcher::class)
-        ->constructor(Di\create(\Pich\App\Responder\BasicResponse::class))
         ->method('addRoute', Di\get('IndexRoute')),
+
 ];
 
