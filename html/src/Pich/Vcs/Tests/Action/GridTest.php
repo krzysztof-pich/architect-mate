1000b170ba4aeb8cf5f0ad27b283ba25bd487564
Krzysztof Pich <kontakt@pich.pl>
2019-12-29 13:02:38 +0100

    Test mock, remove login action

--- html/src/Pich/Vcs/Tests/Action/GridTest.php
+++ html/src/Pich/Vcs/Tests/Action/GridTest.php
@@ -1,22 +1,22 @@
 <?php
 
-namespace Pich\Repository\Tests\Action;
+namespace Pich\Vcs\Tests\Action;
 
+use PHPUnit\Framework\TestCase;
 use Pich\App\JsonResponder;
 use Pich\App\Response\ResponseInterface;
-use Pich\Vcs\Action\Add;
-use PHPUnit\Framework\TestCase;
+use Pich\Vcs\Action\Grid;
 use Phake as p;
 
-class AddTest extends TestCase
+class GridTest extends TestCase
 {
-    public function testAddAction()
+    public function testGridAction()
     {
         // @todo change tests after remove mock
         $response = p::mock(ResponseInterface::class);
         $responder = p::mock(JsonResponder::class);
         p::when($responder)->send(p::anyParameters())->thenReturn($response);
-        $indexAction = new Add($responder);
+        $indexAction = new Grid($responder);
         $indexAction->execute([]);
 
         p::verify($responder)->send(p::anyParameters());
